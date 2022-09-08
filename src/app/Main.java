413be885b3812e613cbe995ad9eb9c5191296d4e
Lowkey <minecraftuser6907@gmail.com>
2022-09-08 09:53:06 +0700

    helper

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,39 @@
 package app;
 
+import java.util.Scanner;
+
+import facade.Helper;
+
 public class Main {
+	Scanner sc = new Scanner(System.in);
+	
+	
+	public static void main(String[] args) {
+		new Main();
+	}
+	
+	
+	public Main() {
+		Helper.showSplashBeginning();
+		
+		int input = 0 ;
+		while(true) {
+			Helper.showMainMenu();
+			input = sc.nextInt(); sc.nextLine();
+			switch (input) {
+			case 1:
+				
+				break;
+			case 2:
+				
+				break;
+			case 3:
+				System.exit(0);
+				break;
+			}
+			
+		}
+		
+	}
 
 }
--- src/facade/Helper.java
+++ src/facade/Helper.java
@@ -1,5 +1,34 @@
 package facade;
 
-public class Helper {
+import java.util.Scanner;
 
+public class Helper {
+	
+	public static void cls() {
+		for(int i = 0 ; i < 33 ; i++) {
+			System.out.println("");
+		}
+	}
+	
+	public static void showSplashBeginning() {
+		cls();
+		System.out.println("  __             __                     ");
+		System.out.println("|/  |           /  |           /        ");
+		System.out.println("|   | ___      (___| ___  ___ (___  ___ ");
+		System.out.println("|   )|   )     |\\   |___)|___ |    |   )");
+		System.out.println("|__/ |__/|     | \\  |__   __/ |__  |__/");
+		System.out.printf("\n\n\n\nPress Enter to Continue...");
+		Scanner sc = new Scanner(System.in);
+		String enter = sc.nextLine();
+	}
+	
+	public static void showMainMenu() {
+		cls();
+		System.out.println("Main Menu");
+		System.out.println("1. Play New Restaurant");
+		System.out.println("2. High Score");
+		System.out.println("3. Exit");
+		System.out.print("Input [1..3] : ");
+	}
+	
 }
